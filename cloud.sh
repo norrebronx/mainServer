cc4b2028295819c76823bb434c7e27ec861ea7ba
norrebronx <leoncio.acevedo@moneysupermarket.com>
2017-06-02 17:14:13 +0100

    first version

---
+++ cloud.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir /root/.ssh
+
+ssh-keygen -f /root/.ssh/id_rsa -P ""
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+
+chmod 600 /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/id_rsa
+
+yum -y install vim-enhanced wget git
+
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+rpm -ivh epel-release-7-9.noarch.rpm
+
+yum -y update
+yum -y install ansible
+
+ansible-galaxy install mongrelion.docker
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+git clone https://github.com/norrebronx/mainServer.git
+
+cd mainServer/ansible
+
+ansible-playbook -i hosts sites.yml
