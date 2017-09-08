d3931c567fe3c2f82c810d7d9ea56846e68429ab
norrebronx <leoncioacevedo@gmail.com>
2017-09-08 19:56:44 +0100

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -10,8 +10,7 @@ chmod 600 /root/.ssh/id_rsa
 
 yum -y install vim-enhanced wget git
 
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-rpm -ivh epel-release-7-9.noarch.rpm
+yum install epel-release
 
 yum -y update
 yum -y install ansible
